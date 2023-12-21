df646e14f51fbb8f26810fc8026661a4672cb750
ujangbedog <domba88galing@gmail.com>
2023-12-21 19:34:54 +0700

    fix error

--- src/app/[locale]/landing-page/page.tsx
+++ src/app/[locale]/landing-page/page.tsx
@@ -1,9 +1,12 @@
 "use client";
 
-import { useState, useEffect } from "react";
+import { useEffect, useState } from "react";
+import { useSearchParams } from "next/navigation";
+
+import { api } from "~/src/trpc/react";
 import { ModalContext } from "~/src/contexts";
 
-import { Footer, Main, Modal, NavigationBar, Loading } from "../components";
+import { Footer, Main, Modal, NavigationBar, Project } from "../components";
 import { Contact, PageSection } from "../components/PageSection";
 
 import {
@@ -19,64 +22,58 @@ import {
 } from "./components/PageSection";
 
 export default function LandingPage() {
-  const [modalContent, setModalContent] = useState<null | React.ReactNode>(null);
-  const [loading, setLoading] = useState(true);
+  const [modalContent, setModalContent] = useState<null | React.ReactNode>(
+    null
+  );
+  const queryParams = useSearchParams();
+  const { data: projects } = api.projects.getAll.useQuery();
 
   useEffect(() => {
-    const fetchData = async () => {
-      try {
-        setLoading(true);
-        // await new Promise(resolve => setTimeout(resolve, 5000));
-        setLoading(false);
-      } catch (error) {
-        console.error("Error fetching data:", error);
-        setLoading(false);
-      }
-    };
-    fetchData();
-  }, []);
-  
+    const project = queryParams.get("project");
+    const projectData = projects?.find(({ id }) => id.toString() === project);
+
+    if (projectData) {
+      setModalContent(<Project data={projectData} />);
+    }
+  }, [queryParams, projects]);
+
   return (
-    <ModalContext.Provider value={{ content: modalContent, setContent: setModalContent }}>
-      {loading ? (
-        // Display a loading component while waiting for data
-        <Loading />
-      ) : (
-        // Render the actual content once loading is false
-        <Main isModalOpen={!!modalContent}>
-          <NavigationBar />
-          <Hero />
-          <PageSection>
-            <Mission />
-          </PageSection>
-          <PageSection>
-            <AboutUs />
-          </PageSection>
-          <PageSection>
-            <Ambassadors />
-          </PageSection>
-          <PageSection bgImage="/bg-numbers.jpg" bgDefaultColor="#6432c8">
-            <Numbers />
-          </PageSection>
-          <PageSection>
-            <Goals />
-          </PageSection>
-          <PageSection
-            bgImage="/bg-projects-and-therapy.jpg"
-            bgDefaultColor="#494a4d"
-          >
-            <WhyUs />
-            <Therapy />
-          </PageSection>
-          <PageSection>
-            <Projects />
-          </PageSection>
-          <PageSection bgDefaultColor="#1c1d20">
-            <Contact />
-          </PageSection>
-          <Footer />
-        </Main>
-      )}
+    <ModalContext.Provider
+      value={{ content: modalContent, setContent: setModalContent }}
+    >
+      <Main isModalOpen={!!modalContent}>
+        <NavigationBar />
+        <Hero />
+        <PageSection>
+          <Mission />
+        </PageSection>
+        <PageSection>
+          <AboutUs />
+        </PageSection>
+        <PageSection>
+          <Ambassadors />
+        </PageSection>
+        <PageSection bgImage="/bg-numbers.jpg" bgDefaultColor="#6432c8">
+          <Numbers />
+        </PageSection>
+        <PageSection>
+          <Goals />
+        </PageSection>
+        <PageSection
+          bgImage="/bg-projects-and-therapy.jpg"
+          bgDefaultColor="#494a4d"
+        >
+          <WhyUs />
+          <Therapy />
+        </PageSection>
+        <PageSection>
+          <Projects />
+        </PageSection>
+        <PageSection bgDefaultColor="#1c1d20">
+          <Contact />
+        </PageSection>
+        <Footer />
+      </Main>
 
       {modalContent && <Modal />}
     </ModalContext.Provider>
