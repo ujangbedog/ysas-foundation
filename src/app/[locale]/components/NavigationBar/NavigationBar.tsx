2ff55eb6d960817336f938b22eaba450428bcbf6
ujangbedog <domba88galing@gmail.com>
2024-01-05 03:23:25 +0700

    editing nav

--- src/app/[locale]/components/NavigationBar/NavigationBar.tsx
+++ src/app/[locale]/components/NavigationBar/NavigationBar.tsx
@@ -2,18 +2,25 @@
 
 import React, { useContext, useEffect, useRef, useState } from "react";
 import { useTranslations } from "next-intl";
+import { useLocale } from "next-intl";
 import { useRouter } from "next-intl/client";
 import Link from "next-intl/link";
 
 import { Button, eButtonColor, eButtonType } from "../Button";
+import { Icon, eIcons } from "../Icon";
 import { Logo, eLogoType } from "../Logo";
 import { LanguageSelection } from "./LanguajeSelection";
 import { Hamburguer } from "./Hamburger";
 
 import styles from "./NavigationBar.module.scss";
 import { MainScrollContext } from "~/src/contexts";
+import { color } from "html2canvas/dist/types/css/types/color";
 
 export const NavigationBar = ({ light = false, scrollThreshold = 220 }) => {
+  const [selectedLocale, setSelectedLocale] = useState(useLocale());
+  const [isDisplayingAbout, setDisplayingAbout] = useState(false);
+  const [isDisplayingInformation, setDisplayingInformation] = useState(false);
+  const [isDisplayingService, setDisplayingService] = useState(false);
   const t = useTranslations("nav");
   const [isScroll, setScroll] = useState(false);
   const [isMenuOpen, setMenuOpen] = useState(false);
@@ -28,11 +35,16 @@ export const NavigationBar = ({ light = false, scrollThreshold = 220 }) => {
   const menuRef = useRef(null);
   const scroll = useContext(MainScrollContext);
 
+  const handleSelect = (locale: string) => {
+    setSelectedLocale(locale);
+    setDisplayingAbout(false);
+  };
+
   const menuItems = [
     { value: "home", link: "/" },
-    { value: "ambassadors", link: "/about" },
-    { value: "contact", link: "/contact" },
-    { value: "faqs", link: "/faqs" },
+    // { value: "about", link: "/about" },
+    // { value: "contact", link: "/contact" },
+    // { value: "faqs", link: "/faqs" },
   ] as const;
 
   const getClassName = () => {
@@ -42,6 +54,15 @@ export const NavigationBar = ({ light = false, scrollThreshold = 220 }) => {
       isScroll ? styles["navigation-bar--on-scroll"] : "",
       isMenuOpen ? styles["navigation-bar--menu-open"] : "",
     ].join(" ");
+  };
+
+  const handleMouseLeave = () => {
+    // Implement the logic to handle mouse leave
+    // For example, you can close all dropdowns by resetting their states
+    setDisplayingAbout(false);
+    setDisplayingInformation(false);
+    setDisplayingService(false);
+    // ... (reset other states for other menu items)
   };
 
   useEffect(() => {
@@ -103,12 +124,22 @@ export const NavigationBar = ({ light = false, scrollThreshold = 220 }) => {
   };
 
   const renderMenu = (modifier = "", isResponsive = false) => {
-    const style = `navigation-bar__menu-items${
-      modifier !== "" ? `--${modifier}` : ""
-    }`;
+    const style = `navigation-bar__menu-items${modifier !== "" ? `--${modifier}` : ""}`;
 
     return (
       <article className={styles[style]} ref={menuRef}>
+
+        {/* <ul>
+          {menuItems.map((item) => {
+            return (
+              <li key={item.value} className={styles[item.value]}>
+                <Link onClick={() => setMenuOpen(false)} href={item.link}>
+                  {t(item.value)}
+                </Link>
+              </li>
+            );
+          })}
+        </ul> */}
         <ul>
           {menuItems.map((item) => {
             return (
@@ -119,8 +150,82 @@ export const NavigationBar = ({ light = false, scrollThreshold = 220 }) => {
               </li>
             );
           })}
+          
+          <div className={styles["menu-selection"]} onMouseLeave={() => handleMouseLeave()}>
+          <li key="about" className={styles["about"]}>
+            <div className="onMouse" onMouseEnter={() => setDisplayingAbout(true)}>
+              <div className={styles["menu-selection__selected"]} onClick={() => setDisplayingAbout(!isDisplayingAbout)}>
+                About Us
+                <Icon icon={eIcons.chevronDown} className={styles["menu-selection__icon"]} />
+              </div>
+            </div>
+          </li>
+          <li>
+            <div className="onMouse" onMouseEnter={() => setDisplayingInformation(true)}>
+              <div className={styles["menu-selection__selected"]} onClick={() => setDisplayingInformation(!isDisplayingInformation)}>
+                Informasi Publik
+                <Icon icon={eIcons.chevronDown} className={styles["menu-selection__icon"]} />
+              </div>
+            </div>
+          </li>
+          <li>
+            <div className="onMouse" onMouseEnter={() => setDisplayingService(true)}>
+              <div className={styles["menu-selection__selected"]} onClick={() => setDisplayingService(!isDisplayingService)}>
+                Layanan Kami
+                <Icon icon={eIcons.chevronDown} className={styles["menu-selection__icon"]} />
+              </div>
+            </div>
+          </li>
+          
+          {isDisplayingAbout && (
+            <div className={styles["menu-selection__list"]} onMouseEnter={() => setDisplayingAbout(true)}>
+              <div className={styles["menu-selection__con"]}>
+                <h1>About</h1>
+                <ul>
+                    <li>
+                      asdasdasd
+                    </li>
+                    <li>
+                      asdasdasd
+                    </li>
+                </ul>
+              </div>
+            </div>
+          )}
+          {isDisplayingInformation && (
+            <div className={styles["menu-selection__list"]} onMouseEnter={() => setDisplayingInformation(true)}>
+              <div className={styles["menu-selection__con"]}>
+                <h1>Informasi Publik</h1>
+                <ul>
+                    <li>
+                      asdasdasd
+                    </li>
+                    <li>
+                      asdasdasd
+                    </li>
+                </ul>
+              </div>
+            </div>
+          )}
+          {isDisplayingService && (
+            <div className={styles["menu-selection__list"]} onMouseEnter={() => setDisplayingService(true)}>
+              <div className={styles["menu-selection__con"]}>
+                <h1>Service</h1>
+                <ul>
+                    <li>
+                      asdasdasd
+                    </li>
+                    <li>
+                      asdasdasd
+                    </li>
+                </ul>
+              </div>
+            </div>
+          )}
+          </div>
         </ul>
 
+        
         {isResponsive && (
           <>
             <LanguageSelection />
@@ -150,6 +255,7 @@ export const NavigationBar = ({ light = false, scrollThreshold = 220 }) => {
   return (
     <header className={getClassName()} ref={headerRef}>
       <div className={styles["navigation-bar__wrapper"]}>
+        
         <Hamburguer
           isOpen={isMenuOpen}
           onClick={() => setMenuOpen((prev) => !prev)}
@@ -161,6 +267,7 @@ export const NavigationBar = ({ light = false, scrollThreshold = 220 }) => {
         >
           <Logo type={logoType} />
         </Link>
+        
         {renderMenu()}
         <section className={styles["navigation-bar__contents"]}>
           <div className={styles["navigation-bar__actions"]}>
